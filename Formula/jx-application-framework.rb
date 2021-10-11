b3cb40556dba8da7092651d3f818b2764663db9d
John Lindal <github@newplanetsoftware.com>
2021-10-10 17:27:01 -0700

    adjust dependencies

--- Formula/jx-application-framework.rb
+++ Formula/jx-application-framework.rb
@@ -4,17 +4,17 @@ class JxApplicationFramework < Formula
   license "LGPL-2.1-or-later"
   head "https://github.com/jafl/jx_application_framework.git", branch: "utf8"
 
-  depends_on "pkg-config" => :build
-  depends_on "ace"        => :build
-  depends_on "icu4c"      => :build
-  depends_on "pcre"       => :build
-  depends_on "re-flex"    => :build
-  depends_on "gd"         => :build
-  depends_on "libx11"     => :build
-  depends_on "libxft"     => :build
-  depends_on "libxpm"     => :build
-  depends_on "freetype"   => :build
-  depends_on "aspell"     => :build
+  depends_on "pkg-config"
+  depends_on "ace"
+  depends_on "icu4c"
+  depends_on "pcre"
+  depends_on "re-flex"
+  depends_on "gd"
+  depends_on "libx11"
+  depends_on "libxft"
+  depends_on "libxpm"
+  depends_on "freetype"
+  depends_on "aspell" => :build
 
   def install
     ENV.deparallelize
