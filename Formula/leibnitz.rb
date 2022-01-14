ca68f67fa44a2c87664989bfd0a9ca5983efb2bc
John Lindal <github@newplanetsoftware.com>
2022-01-13 19:29:27 -0800

    update version

--- Formula/leibnitz.rb
+++ Formula/leibnitz.rb
@@ -1,9 +1,9 @@
 class Leibnitz < Formula
   desc "Leibnitz provides a calculator and the ability to plot functions."
   homepage "https://github.com/jafl/leibnitz"
-  version "3.0.1"
-  url "https://github.com/jafl/leibnitz/releases/download/v3.0.1/leibnitz_3.0.1_macos11_x86_64.tar.gz"
-  sha256 "26bb15d62abf10037f8e497c9569fd763deddce385b6ba2e718c3ee1b751e8f4"
+  version "3.0.2"
+  url "https://github.com/jafl/leibnitz/releases/download/v3.0.2/leibnitz_3.0.2_macos11_x86_64.tar.gz"
+  sha256 "c71e9fa2a595321d5e1d9af77e14a5220a0e89696f2f80911717a86b9a95f719"
   license "GPL-3.0-or-later"
 
   def install
