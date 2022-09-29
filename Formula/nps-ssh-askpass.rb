c5a77a447e689951f7230de72a2cbc590d31ed03
John Lindal <github@newplanetsoftware.com>
2022-09-28 18:04:32 -0700

    update nps-ssh-askpass

--- Formula/nps-ssh-askpass.rb
+++ Formula/nps-ssh-askpass.rb
@@ -2,8 +2,8 @@ class NpsSshAskpass < Formula
   desc "nps-ssh-askpass does exactly the same job as x11-ssh-askpass, but it's not as hard to compile on MacOS, because it doesn't require xmkmf."
   homepage "https://github.com/jafl/ssh-askpass"
   version "1.2.0"
-  url "https://github.com/jafl/ssh-askpass/releases/download/v1.2.0/nps-ssh-askpass_1.2.0_darwin.tar.gz"
-  sha256 "605fc3eaa9ff1c41a6a2e087a9416e696a5b34a86c83f09675e62965a9af85b0"
+  url "https://github.com/jafl/ssh-askpass/releases/download/v1.2.0/nps-ssh-askpass_1.2.0_macos_x86_64.tar.gz"
+  sha256 "958365631201553e824e9ed1f5ad204808dcd5620fff39467a815203eddab83f"
   license "GPL-3.0-or-later"
 
   def install
