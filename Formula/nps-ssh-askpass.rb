8713ca0013b52ff56dc1450045721fb40c9d551c
John Lindal <github@newplanetsoftware.com>
2023-12-30 03:48:02 +0000

    Update All Formulas to latest releases

--- Formula/nps-ssh-askpass.rb
+++ Formula/nps-ssh-askpass.rb
@@ -1,8 +1,8 @@
 class NpsSshAskpass < Formula
   desc "Does exactly the same job as x11-ssh-askpass, but doesn't require xmkmf"
   homepage "https://github.com/jafl/ssh-askpass"
-  url "https://github.com/jafl/ssh-askpass/releases/download/v1.2.1/nps-ssh-askpass_1.2.1_macos13_x86_64.tar.gz"
-  sha256 "12cfd1b3e7cec673853f7792c1fef684c5a9ecb84f799cab2805ff97fe0d6da2"
+  url "https://github.com/jafl/ssh-askpass/releases/download/v1.2.2/nps-ssh-askpass_1.2.1_macos13_x86_64.tar.gz"
+  sha256 "21d8c456840d83ae95108aa94dfe8a38c34dd7950a654cd4a91527bb1b637e51"
   license "GPL-3.0-or-later"
 
   def install
